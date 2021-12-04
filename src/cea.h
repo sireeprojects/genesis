370b5da1ab44880d4479ba3dc1da52b6059ca405
root <root@localhost.localdomain>
2021-12-04 16:14:03 +0530

    updates

--- src/cea.h
+++ src/cea.h
@@ -17,57 +17,57 @@ protected:
     virtual int overflow(int c) ;
 };
 outbuf ob;
-ostream ceaLog(&ob);
+ostream cealogger(&ob);
 
 typedef enum {
-    Low,
-    Full,
-} msgVerbosityLevel;
+    low,
+    full,
+} msg_verbosity_level;
 
 typedef enum {
     V2,
-    Raw
-} frameType;
+    raw
+} frame_type;
 
 typedef enum {
-    MacPreamble,
-    MacDestAddr,
-    MacSrcAddr,
-    MacLen,
-    MacEtherType,
-    MacFcs,
-    Ipv4Version,
-    Ipv4IHL,
-    Ipv4Tos,
-    Ipv4totalLen,
-    Ipv4Id,
-    Ipv4Flags,
-    Ipv4FragOffset,
-    Ipv4TTL,
-    Ipv4Protocol,
-    Ipv4HdrCsum,
-    Ipv4SrcAddr,
-    Ipv4DestAddr,
-    Ipv4Opts,
-    Ipv4Pad,
-    UDPSrcPort,
-    UDPDestPort,
-    UDPlen,
-    UDPCsum,
-    StreamType,
-    StreamPktsPerBurst,
-    StreamBurstPerStream,
-    StreamInterBurstGap,
-    StreamInterStreamGap,
-    StreamStartDelay,
-    StreamRateType,
-    StreamIpg,
-    StreamPercentage,
-    StreamPktsPerSec,
-    StreamBitRate,
-    PayloadType,
-    FieldsSize
-} fieldId;
+    mac_preamble,
+    mac_dest_addr,
+    mac_src_addr,
+    mac_len,
+    mac_ether_type,
+    mac_fcs,
+    ipv4_version,
+    ipv4_ihl,
+    ipv4_tos,
+    ipv4_total_len,
+    ipv4_id,
+    ipv4_flags,
+    ipv4_frag_offset,
+    ipv4_ttl,
+    ipv4_protocol,
+    ipv4_hdr_csum,
+    ipv4_src_addr,
+    ipv4_dest_addr,
+    ipv4_opts,
+    ipv4_pad,
+    udp_src_port,
+    udp_dest_port,
+    udp_len,
+    udp_csum,
+    stream_type,
+    stream_pkts_per_burst,
+    stream_burst_per_stream,
+    stream_inter_burst_gap,
+    stream_inter_stream_gap,
+    stream_start_delay,
+    stream_rate_type,
+    stream_ipg,
+    stream_percentage,
+    stream_pkts_per_aec,
+    stream_bit_rate,
+    payload_type,
+    fields_size
+} field_id;
 
 typedef enum {
     // value modifiers
@@ -79,32 +79,32 @@ typedef enum {
     cincrement,             // starts from 0 and loops at repeat after
     cdecrement,             // starts from ff and loops at repeat after
     // data modifiers
-    incrByte,              // start from zero
-    incrWord,              // start from zero
-    decrByte,              // start from FF
-    decrWord,              // start from FF
-    repeattPattern,        // repeat a data pattern
-    fixedPattern,          // use data pattern only once, rest 0s
+    incr_byte,              // start from zero
+    incr_word,              // start from zero
+    decr_byte,              // start from FF
+    decr_word,              // start from FF
+    repeat_pattern,        // repeat a data pattern
+    fixed_pattern,          // use data pattern only once, rest 0s
     // stream modifiers
-    continuousPkts,            
-    continuousBurst,          
-    stopAfterStream,    
-    gotoNextStream,
+    continuous_pkts,            
+    continuous_burst,          
+    stop_after_stream,    
+    goto_next_stream,
     // stream rate modifiers
     ipg,
     percentage,
-    pktsPerSec,
-    bitRate
-} fieldModifier;
+    pkts_per_sec,
+    bit_rate
+} field_modifier;
 
-struct PACKED ceaField { // 81 bytes
+struct PACKED ceaField {
     bool touched : 1;
     bool merge : 1;
     uint64_t mask : 64;
     uint32_t id : 32;
     uint32_t len: 32;
     uint32_t ofset : 32;
-    fieldModifier modifier : 32;
+    field_modifier modifier : 32;
     uint64_t value: 64;
     uint32_t start: 32;
     uint32_t stop: 32;
@@ -114,13 +114,13 @@ struct PACKED ceaField { // 81 bytes
     char pad[47];
 };
 
-class ceaStream {
+class cea_stream {
 public:
     void set(uint32_t id, uint64_t value);
-    void set(uint32_t id, fieldModifier spec);
+    void set(uint32_t id, field_modifier spec);
     string describe() const;
-    ceaStream();
-    friend ostream& operator << (ostream& os, const ceaStream& cmd);
+    cea_stream();
+    friend ostream& operator << (ostream& os, const cea_stream& cmd);
 private:
     ceaField fld[64];
     void consolidate();
@@ -134,7 +134,7 @@ private:
 namespace cea {
 
 ofstream logfile;
-msgVerbosityLevel global_verbosity;
+msg_verbosity_level global_verbosity;
 
 int outbuf::overflow(int_type c) {
     if (c != EOF) {
@@ -147,21 +147,18 @@ int outbuf::overflow(int_type c) {
     return c;
 }
 
-
-class initLib {
+class init_lib {
 public:
-    initLib() {
+    init_lib() {
         logfile.open("run.log", ofstream::out);
         if (!logfile.is_open()) {
             cout << "Error creating logfile. Aborting..." << endl;
             exit(1);
         }
     }
-    ~initLib() { logfile.close(); }
+    ~init_lib() { logfile.close(); }
 };
 
-
-initLib init_lib;
-
+init_lib init_lib;
 
 } // namespace
