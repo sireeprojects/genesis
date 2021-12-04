1a20d5f5466e77dd30bda736b85adf03fc0a9f0b
root <root@localhost.localdomain>
2021-12-04 16:03:14 +0530

    updates

--- test/test.cpp
+++ test/test.cpp
@@ -1,8 +1,7 @@
 #include "cea.h"
 using namespace cea;
 
-
 int main() {
-    ceaLog << "Size of Single Field: " << dec << sizeof(ceaField) << " bytes" << endl;
+    cealogger << "Size of Single Field: " << dec << sizeof(ceaField) << " bytes" << endl;
     return 0;
 }
