007087cb0dcc106b6bdac3e1888e61d485d8d202
root <root@localhost.localdomain>
2022-02-05 21:58:51 +0530

    updates

---
+++ automation/stream_db/basic.py
@@ -0,0 +1,18 @@
+import os
+import xlrd
+
+# name of the spreadsheet
+fname = "StreamDB.xls"
+
+# open spreadsheet and get handle to the first sheet
+wb = xlrd.open_workbook(fname)
+sheet = wb.sheet_by_index(0)
+
+print("Name of the sheet:", sheet.name)
+print("Number of Rows:", sheet.nrows)
+print("Number of Cols:", sheet.ncols)
+
+idCol = 3 # Column numbering starts with 0
+
+for id in range(1, (sheet.nrows)):
+    print(sheet.cell_value(id, idCol))
