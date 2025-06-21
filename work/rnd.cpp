bbc09686880b6c4130c7fdc86c5eca82186cfd74
sireeprojects <siree.projects@gmail.com>
2025-06-21 06:30:44 +0530

    updates

--- work/rnd.cpp
+++ work/rnd.cpp
@@ -8,28 +8,41 @@ using namespace std;
 struct cea_field_random {
     uint64_t seed;
     mt19937 engine;
-    uniform_int_distribution<> distr;
-    // discrete_distribution<> distr;
+    uniform_int_distribution<> udist;
+    discrete_distribution<> wdist;
 };
 
 int main() {
     random_device rd;
     cea_field_random rnd;
     
-    // rnd.seed = rd();
-    rnd.seed = 12345;
+    rnd.seed = rd();
+    // rnd.seed = 12345;
 
     cout << string(30, '-') << endl;
     cout << "Seed: " << rnd.seed << endl;
 
     rnd.engine.seed(rnd.seed);
 
-    uniform_int_distribution<int>::param_type d2(2, 10);
-    rnd.distr.param(d2);
-    cout << rnd.distr;
+    // WEIGHTED Distribution
+    vector<double>weights = {1.1, 3.3, 0.5, 2.2};
+    discrete_distribution<>::param_type wd(weights.begin(), weights.end());
+    rnd.wdist.param(wd);
+    cout << string(30, '-') << endl;
+    uint32_t idx;
+    for (uint32_t i=0; i<10; i++) {
+        idx = rnd.wdist(rnd.engine);
+        cout << weights[idx] << endl;
+    }
 
+    // UNIFORM Distribution
+    uniform_int_distribution<int>::param_type d2(2, 10);
+    rnd.udist.param(d2);
+    cout << rnd.udist << endl;
+
+    cout << string(30, '-') << endl;
     for (uint32_t i=0; i<10; i++)
-        cout << rnd.distr(rnd.engine) << endl;
+        cout << rnd.udist(rnd.engine) << endl;
 
     return 0;
 }
